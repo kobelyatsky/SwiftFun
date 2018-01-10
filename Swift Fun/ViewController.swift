b8eea7af1dc82ee0cb01a828dc9b67314dea4d4b
Dmitry Kobelyatsky <kobelyatsky@gmail.com>
2018-01-10 12:13:45 +0200

    buttonCounts += 1

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -7,7 +7,7 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        buttonCount = buttonCount + 1
+        buttonCount += 1
         print(buttonCount)
         
         if buttonCount >= 5 {
